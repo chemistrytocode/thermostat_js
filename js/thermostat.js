6679a73b677dc79a6aea5d5b0c2bdae3551ee562
sg1110 <saule.guzyte@tesco.com>
2019-02-07 13:59:36 +0000

    Temperature
    added

--- js/thermostat.js
+++ js/thermostat.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
   var thermostat = new Thermostat()
   updateTemperature()
   updateUsage()
+  get_weather()
 
   $('#temperature-up').click(function () {
     thermostat.up()
@@ -20,6 +21,7 @@ $(document).ready(function () {
     thermostat.reset()
     updateTemperature()
     updateUsage()
+    display_weather()
   })
 
   $('#ps-on').click(function () {
@@ -40,4 +42,10 @@ $(document).ready(function () {
     // $('#usage').text(thermostat.currentUsage())
     $('#usage').attr('class', thermostat.currentUsage())
   }
+
+  function get_weather(){
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=57e55e68828c05bd66fbbe542d6fb8c5&units=metric', function(data) {
+      $('#weather').text("temperature now " + data.main.temp + " c");
+    })}
+
 })
