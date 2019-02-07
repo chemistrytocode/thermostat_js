bb63d7094f18097e73e04485ff93666f49846a8e
sg1110 <saule.guzyte@tesco.com>
2019-02-07 15:51:22 +0000

    Thermostat style changed

--- js/thermostat.js
+++ js/thermostat.js
@@ -36,23 +36,25 @@ $(document).ready(function () {
 
   function updateTemperature () {
     $('#output').text(thermostat.temperature())
-
   };
-  function updateUsage() {
+  function updateUsage () {
     // $('#usage').text(thermostat.currentUsage())
     $('#usage').attr('class', thermostat.currentUsage())
   }
 
-  function default_weather(city = 'London'){
-  $.get('http://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid=57e55e68828c05bd66fbbe542d6fb8c5&units=metric', function(data) {
-      $('#current-temperature').text('Temp in ' + city + ' ' + data.main.temp + 'c');
-    })}
+  function default_weather (city = 'London') {
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=57e55e68828c05bd66fbbe542d6fb8c5&units=metric', function (data) {
+      $('#current-temperature').text('Temp in ' + city + ' ' + data.main.temp + 'c')
+    })
+  }
 
-    $('#current-city').change(function() {
-  var city = $('#current-city').val();
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
-    $('#current-temperature').text('Temp in ' + city + ' ' + data.main.temp + 'c')
+  $('#current-city').change(function () {
+    var city = $('#current-city').val()
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function (data) {
+      $('#current-temperature').text('Temp in ' + city + ' ' + data.main.temp + 'c')
+    })
   })
-})
+
+
 
 })
